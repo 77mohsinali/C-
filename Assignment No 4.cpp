124cf405fed7af20cfb1f53ae1909c5db8f2645e
Mohsin Ali <136420189+77mohsinali@users.noreply.github.com>
2023-12-02 13:48:44 +0500

    User and predefined functions

---
+++ Assignment No 4.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+
+using namespace std;
+
+void sum(int a, int b){
+       cout<<"Sum of a and b :"<<a+b<<endl;
+       
+   }
+void sum( double a , double b){
+     cout<<"Sum of a and b :"<<a+b<<endl;
+       
+   }
+   
+int main(){
+       
+    sum(1.5, 1.4);
+    sum(1, 1);
+    return 0;
+       
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+
